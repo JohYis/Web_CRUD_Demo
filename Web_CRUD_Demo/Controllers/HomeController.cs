fc7c8b404bf3a4475f2a255178c1fb6338286dd5
JohYis <andy80105@gmail.com>
2019-02-14 22:56:42 +0800

    add whole files

---
+++ Web_CRUD_Demo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_CRUD_Demo.Models;
+
+namespace Web_CRUD_Demo.Controllers {
+  public class HomeController : Controller {
+    public ActionResult Index() {
+      return View(model: new Model_EmployeeNumber().GetEmployeeNumber());
+    }
+    public ActionResult Add() {
+      var currentState = "";
+      if (!string.IsNullOrEmpty(Request["txtNumber"]) || !string.IsNullOrEmpty(Request["txtName"])) {
+        if (new Model_EmployeeNumber().InsertEmployeeNumber(int.Parse(Request["txtNumber"]), Request["txtName"]) == 1) {
+          currentState = "1";
+        } else {
+          currentState = "0";
+        }
+      }
+      return View(model: currentState);
+    }
+    public ActionResult Update() {
+      if (!string.IsNullOrEmpty(Request["txtNumber"]) && !string.IsNullOrEmpty(Request["txtName"])) {
+        if (new Model_EmployeeNumber().UpdateEmployeeNumber(number: int.Parse(Request["txtNumber"]), name: Request["txtName"]) == 1) {
+          return RedirectToAction("Index");
+        }
+      }
+      return View(model: new Model_EmployeeNumber().GetEmployeeNumber(int.Parse(Request["RowNumber"])).AsEnumerable().FirstOrDefault());
+    }
+    public string Delete() {
+      return new Model_EmployeeNumber().DeteleEmployeeNumber(int.Parse(Request["number"])).ToString();
+    }
+  }
+}
---
+++ Web_CRUD_Demo/Models/Model_EmployeeNumber.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Web_CRUD_Demo.Models {
+  public class Model_EmployeeNumber {
+    public string strConnectionString = "Data Source=(local);Initial Catalog=AdventureWorksDW2017;Integrated Security=false;User ID=Andy;Password=8885;";
+
+    public int InsertEmployeeNumber(int number = 0, string name = "") {
+      var effected_num = 0;
+      var objConnection = new SqlConnection(strConnectionString);
+      var objCommand = new SqlCommand() {
+        CommandText = @"
+          INSERT INTO EmployeeNumber (
+        	  Number
+        	  ,Name
+          ) VALUES (
+        	  @Number
+        	  ,@Name
+          )
+        "
+,
+        CommandTimeout = 60
+,
+        CommandType = CommandType.Text
+,
+        Connection = objConnection
+      };
+      objCommand.Parameters.Add(new SqlParameter("@Number", number));
+      objCommand.Parameters.Add(new SqlParameter("@Name", name));
+      objConnection.Open();
+      effected_num = objCommand.ExecuteNonQuery();
+      objConnection.Close();
+      objConnection.Dispose();
+      objCommand.Dispose();
+      return effected_num;
+    }
+
+    public DataTable GetEmployeeNumber(int number = 0) {
+      var dtEmployeeNumber = new DataTable();
+      var objConnection = new SqlConnection(strConnectionString);
+      var objCommand = new SqlCommand() {
+        CommandText = @"
+          SELECT *
+          FROM EmployeeNumber
+          WHERE 1=1
+          AND (
+            @Number = 0
+            OR
+            Number = @Number
+          )
+        "
+        ,
+        CommandTimeout = 60
+        ,
+        CommandType = CommandType.Text
+        ,
+        Connection = objConnection
+      };
+      objCommand.Parameters.Clear();
+      objCommand.Parameters.Add(new SqlParameter("@Number", number));
+      objConnection.Open();
+      dtEmployeeNumber.Load(objCommand.ExecuteReader());
+      objConnection.Close();
+      objConnection.Dispose();
+      objCommand.Dispose();
+      return dtEmployeeNumber;
+    }
+
+    public int UpdateEmployeeNumber(int number = 0, string name = "") {
+      var effected_num = 0;
+      var objConnection = new SqlConnection(strConnectionString);
+      var objCommand = new SqlCommand() {
+        CommandText = @"
+          UPDATE EmployeeNumber
+          SET Name = @Name
+          WHERE 1=1
+          AND Number = @Number
+        "
+,
+        CommandTimeout = 60
+,
+        CommandType = CommandType.Text
+,
+        Connection = objConnection
+      };
+      objCommand.Parameters.Clear();
+      objCommand.Parameters.Add(new SqlParameter("@Name", name));
+      objCommand.Parameters.Add(new SqlParameter("@Number", number));
+      objConnection.Open();
+      effected_num = objCommand.ExecuteNonQuery();
+      objConnection.Close();
+      objConnection.Dispose();
+      objCommand.Dispose();
+      return effected_num;
+    }
+
+    public int DeteleEmployeeNumber(int number = 0) {
+      var effected_num = 0;
+      var objConnection = new SqlConnection(strConnectionString);
+      var objCommand = new SqlCommand() {
+        CommandText = @"
+          DELETE FROM EmployeeNumber
+          WHERE 1=1
+          AND Number = @Number
+        "
+,
+        CommandTimeout = 60
+,
+        CommandType = CommandType.Text
+,
+        Connection = objConnection
+      };
+      objCommand.Parameters.Clear();
+      objCommand.Parameters.Add(new SqlParameter("@Number", number));
+      objConnection.Open();
+      effected_num = objCommand.ExecuteNonQuery();
+      objConnection.Close();
+      objConnection.Dispose();
+      objCommand.Dispose();
+      return effected_num;
+    }
+  }
+}
